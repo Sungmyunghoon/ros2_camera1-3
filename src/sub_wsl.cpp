466351abf8eaaa90e322f74f8e4e2d07518339a6
Sungmyunghoon <112747810+Sungmyunghoon@users.noreply.github.com>
2024-05-22 13:15:03 +0900

    Update sub_wsl.cpp

--- src/sub_wsl.cpp
+++ src/sub_wsl.cpp
@@ -4,26 +4,26 @@
 #include <memory>
 #include <functional>
 #include <iostream>
-using std::placeholders::_1;
-cv::VideoWriter video_writer;
+using std::placeholders::_1; // 콜백 함수에 사용
+cv::VideoWriter video_writer; //OpenCV의 비디오 라이터 객체를 전역변수로 선언
 void mysub_callback(rclcpp::Node::SharedPtr node, const sensor_msgs::msg::CompressedImage::SharedPtr msg)
 {
-    cv::Mat frame = cv::imdecode(cv::Mat(msg->data),  cv::IMREAD_COLOR);
-    cv::imshow("frame",frame);
-    cv::waitKey(1);
-    if (!video_writer.isOpened())
+    cv::Mat frame = cv::imdecode(cv::Mat(msg->data),  cv::IMREAD_COLOR); // 이미지를 COLOR로 받는다
+    cv::imshow("frame",frame); // 화면에 이미지를 출력
+    cv::waitKey(1); // imshow 할 때 필요
+    if (!video_writer.isOpened()) // 이미지를 열 수 없으면 중지
     {
         RCLCPP_ERROR(node->get_logger(), "Could not open the output video file for write");
         return;
     }
-    video_writer.write(frame);
-    RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols);
+    video_writer.write(frame); // 비디오 파일로 저장
+    RCLCPP_INFO(node->get_logger(), "Received Image : %s,%d,%d", msg->format.c_str(),frame.rows,frame.cols); //디버깅 및 모니터링 목적으로 사용
 }
-int main(int argc, char* argv[])
+int main(int argc, char* argv[]) // main 함수
 {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("camsub_wsl");
-    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
+    rclcpp::init(argc, argv); // ROS2 초기화
+    auto node = std::make_shared<rclcpp::Node>("camsub_wsl"); // 노드 생성
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort(); // 
     std::function<void(const sensor_msgs::msg::CompressedImage::SharedPtr msg)> fn;
     int frame_width = 640;
     int frame_height = 360;
